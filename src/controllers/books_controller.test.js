1be418562b55a5a18087d7b661ccdea924335b59
James Heggs <james.heggs@techreturners.com>
2021-11-14 22:36:36 +0000

    Use your words

--- src/controllers/books_controller.test.js
+++ src/controllers/books_controller.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('Test Books API endpoint requests', () => {
-  test('GET all books successfully with success status', async () => {
+  test('GET all books responds with success status', async () => {
     // Act
     const res = await request(app).get('/api/v1/books');
 
