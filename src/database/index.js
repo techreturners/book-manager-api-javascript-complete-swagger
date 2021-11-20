be81816fb5964691003a7ec464e7f058b4832a0c
James Heggs <james.heggs@techreturners.com>
2021-11-20 15:46:39 +0000

    Fix issues with running tests for complete example

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,12 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+let sequelize = new Sequelize('sqlite::memory:');
+
+if (process.env.NODE_ENV !== 'test') {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-});
+    dialect: process.env.DB_DIALECT,
+  });
+}
 
 module.exports = sequelize;
--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ require('dotenv').config({path: `./.env.${environment}`});
 const app = require('./app');
 const populateDummyData = require('./database/database_seed');
 
-app.listen(process.env.APPLICATION_PORT, () => {
-  console.log(`Express started on port ${process.env.APPLICATION_PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Express started on port ${process.env.PORT}`);
 
   // Seed the database with some data
   if (environment === 'dev') {
